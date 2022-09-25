62b04a98d97d3e4333ceeae5ac689022f7b27d24
Piotr Kurzak <piotr.kurzak@gmail.com>
2022-09-25 10:07:31 +0200

    Added notice models (Notice, Condition, Type, Category)

--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+from accounts.models import CustomUser
+
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Condition(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Notice(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    image = models.ImageField(blank=True)
+    price = models.IntegerField()
+    pub_date = models.DateField()
+    is_active = models.BooleanField(blank=True)
+    type_id = models.ForeignKey(Type, on_delete=models.DO_NOTHING)
+    category_id = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    condition_id = models.ForeignKey(Condition, on_delete=models.DO_NOTHING)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
