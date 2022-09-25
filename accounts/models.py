515cd0df060f9ba3f0ecaab75d16c8810a0ed760
Piotr Kurzak <piotr.kurzak@gmail.com>
2022-09-25 09:37:35 +0200

    Added CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    postal_code = models.CharField(max_length=6, blank=True)
+    town_name = models.CharField(max_length=32, blank=True)
