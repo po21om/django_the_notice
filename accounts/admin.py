6442411451650a8ad25644538d1400d286bd12e8
Piotr Kurzak <piotr.kurzak@gmail.com>
2022-09-25 09:40:18 +0200

    Added admin configuration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.models import CustomUser
+
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'is_staff',
+        'phone_number', 'postal_code', 'town_name'
+        )
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'phone_number', 'postal_code', 'town_name')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+            )
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'password1', 'password2')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'phone_number', 'postal_code', 'town_name')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+            )
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
