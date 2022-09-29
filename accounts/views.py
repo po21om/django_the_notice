a37de3f0ee4ee73be5007395405b28100f376708
Piotr Kurzak <piotr.kurzak@gmail.com>
2022-09-29 19:43:55 +0200

    Added proposal for views classes

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.views.generic import FormView
+
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    pass
+
+
+class RegisterView(FormView):
+    pass
+
--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
+
 
 # Create your views here.
+
+
+class NoticeList(ListView):
+    pass
+
+
+class NoticeDetail(DetailView):
+    pass
+
+
+class NoticeOwnDetail(LoginRequiredMixin, DetailView):
+    pass
+
+
+class NoticeCreate(LoginRequiredMixin, CreateView):
+    pass
+
+
+class NoticeUpdate(LoginRequiredMixin, UpdateView):
+    pass
+
+
+class NoticeDelete(LoginRequiredMixin, DetailView):
+    pass
+
