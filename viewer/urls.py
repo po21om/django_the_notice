03c285a4f87f4a057cb2ef2a807bccccb2c15a30
Piotr Kurzak <piotr.kurzak@gmail.com>
2022-10-02 09:53:36 +0200

    Added notice detail template

--- viewer/urls.py
+++ viewer/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path("",NoticeList.as_view(), name="notices"),
     path("my/",NoticeOwnList.as_view(), name="own_notices"),
-    #path("notice/<int:pk>",NoticeDetail.as_view(), name="notice"),
+    path("notice/<int:pk>",NoticeDetail.as_view(), name="notice"),
     #path("notice-create/",NoticeCreate.as_view(), name="notice-create"),
     #path("notice-update/<int:pk>",NoticeUpdate.as_view(), name="notice-update"),
     #path("notice-delete/<int:pk>",NoticeDelete.as_view(), name="notice-delete"),
