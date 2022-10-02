b9bbc5ea006220e59992719f92d6ae42b27f2732
Piotr Kurzak <piotr.kurzak@gmail.com>
2022-10-02 12:25:22 +0200

    Image view if image present & update/edit link available for notices on own_notice_list

--- viewer/urls.py
+++ viewer/urls.py
@@ -1,12 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import NoticeList, NoticeDetail, NoticeCreate, NoticeUpdate, NoticeDelete, NoticeOwnList
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("",NoticeList.as_view(), name="notices"),
-    path("my/",NoticeOwnList.as_view(), name="own_notices"),
-    path("notice/<int:pk>",NoticeDetail.as_view(), name="notice"),
-    path("notice-create/",NoticeCreate.as_view(), name="notice-create"),
-    path("notice-update/<int:pk>",NoticeUpdate.as_view(), name="notice-update"),
-    path("notice-delete/<int:pk>",NoticeDelete.as_view(), name="notice-delete"),
+    path("", NoticeList.as_view(), name="notices"),
+    path("my/", NoticeOwnList.as_view(), name="own_notices"),
+    path("notice/<int:pk>", NoticeDetail.as_view(), name="notice"),
+    path("notice-create/", NoticeCreate.as_view(), name="notice-create"),
+    path("notice-update/<int:pk>", NoticeUpdate.as_view(), name="notice-update"),
+    path("notice-delete/<int:pk>", NoticeDelete.as_view(), name="notice-delete"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
